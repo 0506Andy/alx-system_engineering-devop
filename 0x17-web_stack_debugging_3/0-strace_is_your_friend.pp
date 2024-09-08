b0001d82e8d31396f36d2d16e1410d5e4ceee5e8
0506Andy <05thandy@gmail.com>
2024-09-08 22:59:02 +0000

    corrections

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# A puppet code that fix a word press site of 5xx error to 200k
+# A puppet code that fix a word press site of 5xx error 
 
-exec { 'fix-wordpress-server-error':
-   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path  => '/usr/bin/:/bin/',
+exec { 'fix-word press':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
